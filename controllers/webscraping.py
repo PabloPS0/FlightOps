df21f9322771b19dd6247d2ac92c6ac556239880
Pablo <pablopierry98@gmail.com>
2024-11-08 11:23:40 -0300

    initial commit

---
+++ controllers/webscraping.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# Launch the browser (Chrome or Firefox)
+driver = webdriver.Chrome() 
+driver.get("https://tycoon.airlines-manager.com") # Navigate to the URL
